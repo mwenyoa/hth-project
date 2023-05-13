1ab64a5acb95e9124be555cb96e200fdb2042e87
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-05-13 16:06:46 +0200

    define frontend routes of the app

--- app/javascript/entrypoints/App.tsx
+++ app/javascript/entrypoints/App.tsx
@@ -1,4 +1,11 @@
 import React, { lazy, Suspense} from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Footer from './components/footer'
+import Header from './components/header'
+
+
+
+const CreateOrganization = lazy(() => import('./components/Organizzation/createOrganization'))
 
 
 type Props = {}
@@ -7,7 +14,15 @@ const App = (props: Props) => {
 
   return (
       <>
-
+       <Header />
+        <main>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/create-organization" element={<CreateOrganization />} />
+          </Routes>
+        </Suspense>
+        </main>
+        <Footer />
       </>
   )
 }
