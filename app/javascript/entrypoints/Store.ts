e9c4c6c0fd5f32598154aa84312ffe8f95560775
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-05-22 20:21:29 +0200

    Update store with new reducer state slices

--- app/javascript/entrypoints/Store.ts
+++ app/javascript/entrypoints/Store.ts
@@ -1,9 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-import organizationSlice from "./Redux/Reducers/createOrganization"
+import organizationSlice from "./Redux/Reducers/Organization/createOrganization"
+import organizationsSlice from './Redux/Reducers/Organization/fetchOrganizations';
+import historiesReducer from './Redux/Reducers/History/histories';
+import historySlice from './Redux/Reducers/History/getHistory';
+
+
 
 const store = configureStore({
   reducer: {
-      organization: organizationSlice
+      organization: organizationSlice,
+      organizations:organizationsSlice,
+      histories:historiesReducer,
+      history:historySlice,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false,
