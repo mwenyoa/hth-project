45d2ee5891786d4cf930ba719346e6be2b941ca4
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-05-23 23:51:38 +0200

    Generate donations migration

---
+++ db/migrate/20230523203526_create_donations.rb
@@ -0,0 +1,12 @@
+class CreateDonations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :donations do |t|
+      t.string :purpose
+      t.integer :amount
+      t.references :organization, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
