0555bd0c0941d8b736417b0d11f076ae6d8f2c34
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-05-24 23:57:24 +0200

    Create work place services

---
+++ app/javascript/entrypoints/Redux/Services/workplace.ts
@@ -0,0 +1,85 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+type Workplace = {
+  id: number;
+  workplaceData: {
+    name: string;
+    description: string;
+    organization_id: string;
+    quicknote: string;
+  };
+};
+
+export const fetchWorkplace = createAsyncThunk(
+  "workplace/fetchWorkplace",
+  async (id: number) => {
+    try {
+      const res: any = await axios.get(`/api/v1/workplaces/${id}`);
+        const dt: any = await res.data;
+        console.log("Workplace data", dt);
+        return dt;
+    } catch (e) {
+      throw new Error(e.response.data.error);
+    }
+  }
+);
+
+export const updateWorkplace = createAsyncThunk(
+  "workplace/updateWorkplace",
+  async (workplace: Workplace) => {
+    try {
+      const res = await axios.put(
+        `/api/v1/workplaces/${workplace.id}`,
+        workplace
+      );
+      const dt = await res.data;
+      console.log("Workplace data", dt);
+      return dt;
+    } catch (e) {
+      throw new Error(e.response.data.error);
+    }
+  }
+);
+
+export const createWorkplace = createAsyncThunk(
+  "workplace/createWorkplace",
+  async (workplace: Workplace) => {
+    try {
+      const res = await axios.post(`/api/v1/workplaces`, workplace);
+      const dt = await res.data;
+      console.log("Workplace data", dt);
+      return dt;
+    } catch (e) {
+      throw new Error(e.response.data.error);
+    }
+  }
+);
+
+export const deleteWorkplace = createAsyncThunk(
+  "workplace/deleteWorkplace",
+  async (id: number) => {
+    try {
+      const res = await axios.delete(`/api/v1/workplaces/${id}`);
+      const dt = await res.data;
+      console.log("Workplace data", dt);
+      return dt;
+    } catch (e) {
+      throw new Error(e.response.data.error);
+    }
+  }
+);
+
+export const fetchWorkplaces = createAsyncThunk(
+  "workplace/fetchWorkplaces",
+  async () => {
+    try {
+      const res = await axios.get(`/api/v1/workplaces`);
+      const dt = await res.data;
+      console.log("Workplace data", dt);
+      return dt;
+    } catch (e) {
+      throw new Error(e.response.data.error);
+    }
+  }
+);
