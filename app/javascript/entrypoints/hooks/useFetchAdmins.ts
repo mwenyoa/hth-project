e11204f5b0e3beb0cef59c639de159c9206b5401
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-06-10 22:41:21 +0200

    create hook to fetch admins

---
+++ app/javascript/entrypoints/hooks/useFetchAdmins.ts
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState, AppDispatch } from "../Store";
+import { getAdmins } from "../Redux/Services/admin";
+
+type Props = {};
+
+const useFetchAdmins = (props: Props) => {
+  const dispatch: AppDispatch = useDispatch();
+  const { admins, isLoading } = useSelector((state: RootState) => state.admins);
+  useEffect(() => {
+    if(admins.length === 0){
+      dispatch(getAdmins(props));
+    }
+  }, [admins.length, dispatch]);
+  return {isLoading, admins}
+};
+
+export default useFetchAdmins;
