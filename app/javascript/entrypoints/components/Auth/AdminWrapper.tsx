c3e608d7cb66dae645a53e6a1f1dac90b177932c
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-06-10 22:21:04 +0200

    create admin Wrapper

---
+++ app/javascript/entrypoints/components/Auth/AdminWrapper.tsx
@@ -0,0 +1,17 @@
+import React, {FC, useState } from 'react'
+
+type Props = {}
+
+const AdminWrapper = (Component: FC<any>) => (props: Props) => {
+    const [toggleNav, setToggleNav] = useState<boolean>(false)
+    const navToggleHandler = () => {
+      setToggleNav(prev => !prev);
+    }
+  return (
+    <div>
+        <Component {...props} toggler={toggleNav}/>
+    </div>
+  )
+}
+
+export default AdminWrapper;
