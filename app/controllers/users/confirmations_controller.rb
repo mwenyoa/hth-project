2d64160624458a0676f64978f3a803bf46f8d35c
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-06-10 22:10:10 +0200

    Create confirmations controller

---
+++ app/controllers/users/confirmations_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Users::ConfirmationsController < Devise::ConfirmationsController
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+    if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+      redirect_to "#{ENV['HOST']}/#/account-confirmation", allow_other_host: true
+    else
+      redirect_to "#{ENV['HOST']}/#/account-confirmation-errors", allow_other_host: true
+    end
+  end
+end
