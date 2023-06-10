873a033bbed24f0a07cc3b9770f0ab454a4cec66
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-06-10 23:29:20 +0200

    authenticate app

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# User.create!(firstname: 'Anthony', lastname: 'Mwenyo', email: 'mwenyoanthony@gmail.com',
-#              password_digest: 'password123', bio: 'I am a fullstack engineer')
-
-Organization.create!(name: 'Mwenyo Foundation', logo: 'https://res.cloudinary.com/dq7uyauun/image/upload/v1628589853/Screen_Shot_2021-08-10_at_11.03.37_PM_ewqjxg.png', mission: 'To help the needy', vision: 'To help the needy')
+# Organization.create(name: 'Mwenyo Foundation', logo: 'https://res.cloudinary.com/dq7uyauun/image/upload/v1628589853/Screen_Shot_2021-08-10_at_11.03.37_PM_ewqjxg.png', mission: 'To help the needy', vision: 'To help the needy')
+# User.create(firstname: 'test1', lastname: 'lastname_test', email: 'test@gmail.com',  password_digest:'password123')
+fu = User.first
+fu.role = 'admin'
+fu.save
