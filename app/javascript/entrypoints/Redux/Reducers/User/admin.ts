f821f17e3c49449cd4382ad66797a27cfd7423ef
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-06-12 23:18:17 +0200

    Add Reducer to  manage the fetching of a singleadmin state

--- app/javascript/entrypoints/Redux/Reducers/User/admin.ts
+++ app/javascript/entrypoints/Redux/Reducers/User/admin.ts
@@ -1,66 +1,44 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { getAdmins, createAdmin, deleteAdmin } from "../../Services/admin";
+import { getAdmin } from "../../Services/admin";
 
-interface adminState {
-  admins: any;
-  isLoading: "idle" | "pending" | "loaded" | "failed";
-  error: string | undefined;
-}
-
-const initialState: adminState = {
-  admins: [],
-  isLoading: "idle",
-  error: undefined,
+type adminInfo = {
+  id: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+  photo: string;
+  created_at: Date;
+  confirmed_at: Date;
 };
 
- const adminsReducer = createSlice({
-  name: "System Admin",
+type adminState = {
+  admin: adminInfo;
+  isLoading: "idle" | "pending" | "loaded" | "failed";
+  error: string | undefined;
+};
+
+const initialState = {
+  admin: {},
+  isLoading: "idle",
+  error: undefined,
+} as adminState;
+
+const adminReducer = createSlice({
+  name: "Get Admin",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(getAdmins.pending, (state) => {
+    builder.addCase(getAdmin.pending, (state) => {
       state.isLoading = "pending";
     });
-    builder.addCase(
-      getAdmins.fulfilled,
-      (state, { payload }: PayloadAction) => {
-        state.admins = payload as any;
-        state.isLoading = "loaded";
-      }
-    );
-    builder.addCase(getAdmins.rejected, (state, action) => {
+    builder.addCase(getAdmin.fulfilled, (state, action) => {
+      state.isLoading = "loaded"
+      state.admin = action.payload;
+    });
+    builder.addCase(getAdmin.rejected, (state, action) => {
       state.error = action.error.message;
-      state.isLoading = "failed";
     });
-    builder.addCase(createAdmin.pending, (state) => {
-      state.isLoading = "pending";
-    });
-    builder.addCase(
-      createAdmin.fulfilled,
-      (state, { payload }: PayloadAction) => {
-        state.admins = [...state.admins, payload];
-        state.isLoading = 'loaded';
-      }
-    );
-    builder.addCase(createAdmin.rejected, (state, action) => {
-        state.error = action.error.message;
-        state.isLoading = 'failed'
-    })
-    builder.addCase(deleteAdmin.pending, (state) => {
-        state.isLoading = 'pending';
-    })
-    builder.addCase(deleteAdmin.fulfilled, (state, action) => {
-        const admins = state.admins.filter((admin: any) => admin.id !== action.payload)
-        state.admins = admins;
-        state.isLoading = 'loaded';
-    })
-    builder.addCase(deleteAdmin.rejected, (state, action) => {
-        state.error = action.error.message;
-        state.isLoading = 'failed';
-    })
   },
 });
 
-export default adminsReducer.reducer;
-
-
+export default adminReducer.reducer;
